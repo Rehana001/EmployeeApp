d0a3d101dcd379244ba0e3cd7a08978aef0f850a
Rehana Riaz Noorani <rehanaNoorani3@gmail.com>
2023-11-01 17:01:57 +0500

    home

--- EmployeeApp/screens/Home.js
+++ EmployeeApp/screens/Home.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import { Card } from 'react-native-paper';
 
 function Home(){
     return (
-        <Text>Hello from home</Text>
+        <Card style={styles.mycard}>
+            <View style={styles.Viewcard}>
+            <Image
+            style={{width:60,height:60, borderRadius:30}}
+            source={{uri:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}}
+            />
+            <Text>Hello from home</Text>
+            </View>
+        </Card>
+        
     )
 }
+
+const styles= StyleSheet.create({
+    mycard:{ 
+        margin:20,
+        marginTop:50,
+        padding:5
+    },
+    Viewcard:{
+        flexDirection:"row",
+    }
+})
 
 export default Home;
