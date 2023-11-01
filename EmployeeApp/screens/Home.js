1acb64a0f01a52ac9a2942ba7ee135c14d74b732
Rehana Riaz Noorani <rehanaNoorani3@gmail.com>
2023-11-01 16:06:02 +0500

    project setup

---
+++ EmployeeApp/screens/Home.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+function Home(){
+    return (
+        <Text>Hello from home</Text>
+    )
+}
+
+export default Home;
