ff5416ec2466eafdeac4d4b7dc9cf5d875451111
Rehana Riaz Noorani <76520232+Rehana001@users.noreply.github.com>
2023-12-21 21:22:02 +0500

    push

--- EmployeeApp/App.js
+++ EmployeeApp/App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home';
 import CreateEmployee from './screens/CreateEmployee';
+import Profile from './screens/Profile';
 
 export default function App() {
   return (
     <View style={styles.container}>
    {/* <Home/> */}
-    <CreateEmployee/>
+    {/* <CreateEmployee/> */}
+    <Profile/>
     </View>
   );
 }
---
+++ EmployeeApp/screens/Profile.js
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,Image,Modal} from 'react-native';
+
+const Profile =()=>{
+    <Text>Hello to Profile page </Text>
+}
+
+export default Profile;
